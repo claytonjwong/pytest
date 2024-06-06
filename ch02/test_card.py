8da471ca85bdbce7f8f9de04deed6d60f1a57683
claytonjwong <claytonjwong@gmail.com>
2024-06-06 09:54:49 -0700

    writing test functions - assertion helper functions

--- ch02/test_card.py
+++ ch02/test_card.py
@@ -50,3 +50,24 @@ def test_to_dict():
         'id': 123
     }
     assert c2 == c2_expected
+
+#
+# assertion helper functions
+#
+import pytest
+
+def assert_identical(c1: Card, c2: Card):
+    __tracebackhide__ = True
+    assert c1 == c2
+    if c1.id != c2.id:
+        pytest.fail(f'different ids: {c1.id} != {c2.id}')
+
+def test_identical():
+    c1 = Card('foo', id=123)
+    c2 = Card('foo', id=123)
+    assert_identical(c1, c2)
+
+def test_identical_fail():
+    c1 = Card('foo', id=123)
+    c2 = Card('foo', id=456)
+    assert_identical(c1, c2)
