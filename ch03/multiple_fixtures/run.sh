43d9b0f0c90b95e9d11135d2e69f865cb669437d
claytonjwong <claytonjwong@gmail.com>
2024-06-10 11:50:16 -0700

    pytest fixtures - using multiple fixture levels, ie. test results for adding database records should NOT be dependent upon the order of test execution

---
+++ ch03/multiple_fixtures/run.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+pytest --setup-show
+
+# (venv) ➜  multiple_fixtures git:(main) ✗ pytest --setup-show
+# ================================================================= test session starts ==================================================================
+# platform darwin -- Python 3.12.3, pytest-8.2.1, pluggy-1.5.0
+# rootdir: /Users/claytonjwong/sandbox/pytest/ch03/multiple_fixtures
+# collected 3 items
+
+# test_count.py
+# SETUP    S db
+#         SETUP    F cards_db (fixtures used: db)
+#         test_count.py::test_empty (fixtures used: cards_db, db).
+#         TEARDOWN F cards_db
+#         SETUP    F cards_db (fixtures used: db)
+#         test_count.py::test_two (fixtures used: cards_db, db).
+#         TEARDOWN F cards_db
+#         SETUP    F cards_db (fixtures used: db)
+#         test_count.py::test_three (fixtures used: cards_db, db).
+#         TEARDOWN F cards_db
+# TEARDOWN S db
+
+# ================================================================== 3 passed in 0.00s ===================================================================
