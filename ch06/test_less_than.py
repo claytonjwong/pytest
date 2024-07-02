a63a6ccfb7d02e2ddf4abfa1504a11dcb50fd5d1
claytonjwong <claytonjwong@gmail.com>
2024-07-02 10:03:16 -0700

    markers - skipping tests conditionally with pytest.mark.skipif

--- ch06/test_less_than.py
+++ ch06/test_less_than.py
@@ -1,8 +1,16 @@
+import cards
 import pytest
 from cards import Card
+from packaging.version import parse
 
 @pytest.mark.skip(reason='operator < is not yet supported')
 def test_less_than():
+    c1 = Card('a task')
+    c2 = Card('b task')
+    assert c1 < c2
+
+@pytest.mark.skipif(parse(cards.__version__).major < 2, reason='operator < not supported in version 1.x')
+def test_less_than2():
     c1 = Card('a task')
     c2 = Card('b task')
     assert c1 < c2
